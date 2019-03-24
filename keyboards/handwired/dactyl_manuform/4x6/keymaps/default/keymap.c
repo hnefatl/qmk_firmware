01714ee91a9e7563e963c0eee9a9dc399588d513
Keith <hnefatl@gmail.com>
2019-03-24 00:43:51 +0000

    Fixed right hand thumb cluster rotation

--- keyboards/handwired/dactyl_manuform/4x6/keymaps/default/keymap.c
+++ keyboards/handwired/dactyl_manuform/4x6/keymaps/default/keymap.c
@@ -35,6 +35,7 @@ enum {
 #define SYMBOLS MO(LAYER_SYMBOLS)
 #define SYMBLS SYMBOLS
 
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 // Visual layouts at http://www.keyboard-layout-editor.com
@@ -44,18 +45,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______,KC_SCLN,KC_Q,   KC_J,   KC_K,   KC_X,               KC_B,    KC_M,  KC_W,   KC_V,   KC_Z,   KC_SLSH,   \
                     _______,_______,                                            _______,_______,                   \
                                 KC_SESC,KC_SPC,                     KC_ENT, KC_BSPC,                               \
-                                    SYMBLS,KC_LALT,             SYMBLS, KC_LCTL,                                   \
-                                    KC_LGUI,KC_RGUI,            _______,KC_ESC                                     \
+                                    SYMBLS,KC_LALT,             KC_LCTL, SYMBLS,                                   \
+                                    KC_LGUI,KC_RGUI,            KC_ESC, _______                                    \
 ),
 
 [LAYER_QWERTY] = LAYOUT(
     KC_RST,  KC_Q,  KC_W,   KC_E,   KC_R,   KC_T,               KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_UNDS,   \
     KC_TAB,  KC_A,  KC_S,   KC_D,   KC_F,   KC_G,               KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_COLN,   \
     _______, KC_Z,  KC_X,   KC_C,   KC_V,   KC_B,               KC_N,   KC_M,   KC_COMM,KC_DOT, KC_BSLS,KC_SLSH,   \
-                    KC_LBRC,KC_RBRC,                                            KC_PLUS,KC_EQL,                    \
+                    _______,_______,                                            _______,_______,                   \
                                 KC_SESC,KC_SPC,                     KC_ENT, KC_BSPC,                               \
-                                    SYMBLS,KC_LALT,             SYMBLS, KC_LCTL,                                   \
-                                    KC_LGUI,KC_RGUI,            _______,KC_ESC                                     \
+                                    SYMBLS,KC_LALT,             KC_LCTL, SYMBLS,                                   \
+                                    KC_LGUI,KC_RGUI,            KC_ESC, _______                                    \
 ),
 
 [LAYER_SYMBOLS] = LAYOUT(
@@ -74,8 +75,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,            XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,   \
                     XXXXXXX,XXXXXXX,                                            XXXXXXX,XXXXXXX,                   \
                                 KC_VOLD,KC_VOLU,                    KC_BRIU,KC_BRID,                               \
-                                    KC_MPLY,_______,            XXXXXXX,XXXXXXX,                                   \
-                                    KC_MUTE,QWERTY,             DVORAK, XXXXXXX                                    \
+                                    KC_MPLY,QWERTY,             DVORAK, XXXXXXX,                                   \
+                                    KC_MUTE,XXXXXXX,            XXXXXXX,XXXXXXX                                    \
 )
 };
 
