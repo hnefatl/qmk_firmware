e10ee21e1c68db4e05c67bff888ce1ef5e567e7e
etherandrius <etherandrius@gmail.com>
2019-03-24 20:51:06 +0200

    moved keith custom keybindings to keith dir, restored old default

--- keyboards/handwired/dactyl_manuform/4x6/keymaps/default/keymap.c
+++ keyboards/handwired/dactyl_manuform/4x6/keymaps/default/keymap.c
@@ -2,99 +2,86 @@
 
 extern keymap_config_t keymap_config;
 
-// Layers
-enum {
-    LAYER_DVORAK = 0,
-    LAYER_QWERTY,
-    LAYER_SYMBOLS,
-    LAYER_FUNCTION,
-};
-
-// Custom keycodes
-enum {
-    KC_CUSTOM_RESET = SAFE_RANGE
-};
-#define KC_CRST KC_CUSTOM_RESET
+#define _BASE 0
+#define _RAISE 1
+#define _LOWER 2
 
 // Fillers to make layering more clear
 
-// Left shift when held, caps lock when tapped
-#define KC_SCL LSFT_T(KC_CAPSLOCK)
-// Left shift when held, escape when tapped
-#define KC_SESC LSFT_T(KC_ESCAPE)
+#define ____ KC_TRNS
 
-// Escape when tapped, switch to function layer when held
-#define KC_RST LT(LAYER_FUNCTION, KC_ESCAPE)
+#define SFT_ESC  SFT_T(KC_ESC)
+#define CTL_BSPC CTL_T(KC_BSPC)
+#define ALT_SPC  ALT_T(KC_SPC)
+#define SFT_ENT  SFT_T(KC_ENT)
 
-// "£"
-#define KC_PND UC(0x00A3)
+#define KC_ML KC_MS_LEFT
+#define KC_MR KC_MS_RIGHT
+#define KC_MU KC_MS_UP
+#define KC_MD KC_MS_DOWN
+#define KC_MB1 KC_MS_BTN1
+#define KC_MB2 KC_MS_BTN1
 
-// Layer switches
-#define QWERTY DF(LAYER_QWERTY)
-#define DVORAK DF(LAYER_DVORAK)
-#define SYMBOLS MO(LAYER_SYMBOLS)
-#define SYMBLS SYMBOLS
-
+#define RAISE MO(_RAISE)
+#define LOWER MO(_LOWER)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-// Visual layouts at http://www.keyboard-layout-editor.com
-[LAYER_DVORAK] = LAYOUT(
-    KC_RST, KC_QUOT,KC_COMM,KC_DOT, KC_P,   KC_Y   ,            KC_F,    KC_G,  KC_C,   KC_R,   KC_L,   KC_UNDS,   \
-    KC_TAB, KC_A,   KC_O,   KC_E,   KC_U,   KC_I,               KC_D,    KC_H,  KC_T,   KC_N,   KC_S,   KC_COLN,   \
-    _______,KC_SCLN,KC_Q,   KC_J,   KC_K,   KC_X,               KC_B,    KC_M,  KC_W,   KC_V,   KC_Z,   KC_SLSH,   \
-                    _______,_______,                                            _______,_______,                   \
-                                KC_SESC,KC_SPC,                     KC_ENT, KC_BSPC,                               \
-                                    SYMBLS,KC_LALT,             KC_LCTL, SYMBLS,                                   \
-                                    KC_LGUI,KC_RGUI,            KC_ESC, _______                                    \
+/* Base (qwerty)
+ * +-----------------------------------------+                             +-----------------------------------------+
+ * | ESC  |   q  |   w  |   e  |   r  |   t  |                             |   y  |   u  |   i  |   o  |   p  |      |
+ * |------+------+------+------+------+------|                             |------+------+------+------+------+------|
+ * | TAB  |   a  |   s  |   d  |   f  |   g  |                             |   h  |   j  |   k  |   l  |   ;  |      |
+ * |------+------+------+------+------+------|                             |------+------+------+------+------+------|
+ * | SHFT |   z  |   x  |   c  |   v  |   b  |                             |   n  |   m  |   ,  |   .  |   /  |      |
+ * +------+------+------+------+-------------+                             +-------------+------+------+------+------+
+ *               |  [   |   ]  |                                                         |      |      |
+ *               +-------------+-------------+                             +-------------+-------------+
+ *                             |      |      |                             |      |      |
+ *                             |------+------|                             |------+------|
+ *                             |      |      |                             |      |      |
+ *                             +-------------+                             +-------------+
+ *                                           +-------------+ +-------------+
+ *                                           |      |      | |      |      |
+ *                                           |------+------| |------+------|
+ *                                           |      |      | |      |      |
+ *                                           +-------------+ +-------------+
+ */
+
+[_BASE] = LAYOUT( \
+    KC_ESC,  KC_Q,  KC_W,   KC_E,   KC_R,   KC_T,               KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_MINS,   \
+    KC_TAB,  KC_A,  KC_S,   KC_D,   KC_F,   KC_G,               KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,   \
+    KC_LSFT, KC_Z,  KC_X,   KC_C,   KC_V,   KC_B,               KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,KC_BSLASH, \
+                    KC_LBRC,KC_RBRC,                                            KC_PLUS,KC_EQL,                    \
+                                    RAISE,  KC_SPC,             KC_ENT, LOWER,                                     \
+                                    KC_TAB, KC_HOME,            KC_END, KC_DEL,                                    \
+                                    KC_BSPC,KC_GRV,             KC_LGUI,KC_LALT                                    \
 ),
 
-[LAYER_QWERTY] = LAYOUT(
-    KC_RST,  KC_Q,  KC_W,   KC_E,   KC_R,   KC_T,               KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_UNDS,   \
-    KC_TAB,  KC_A,  KC_S,   KC_D,   KC_F,   KC_G,               KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_COLN,   \
-    _______, KC_Z,  KC_X,   KC_C,   KC_V,   KC_B,               KC_N,   KC_M,   KC_COMM,KC_DOT, KC_BSLS,KC_SLSH,   \
-                    _______,_______,                                            _______,_______,                   \
-                                KC_SESC,KC_SPC,                     KC_ENT, KC_BSPC,                               \
-                                    SYMBLS,KC_LALT,             KC_LCTL, SYMBLS,                                   \
-                                    KC_LGUI,KC_RGUI,            KC_ESC, _______                                    \
+[_LOWER] = LAYOUT(
+    _______,_______,_______,_______,_______,KC_LBRC,            KC_RBRC, KC_P7, KC_P8,  KC_P9,  RESET,  KC_PLUS,   \
+    _______,KC_HOME,KC_PGUP,KC_PGDN,KC_END ,KC_LPRN,            KC_RPRN, KC_P4, KC_P5,  KC_P6,  KC_MINS,KC_PIPE,   \
+    _______,_______,_______,_______,_______,_______,            _______, KC_P1, KC_P2,  KC_P3,  KC_EQL, KC_UNDS,   \
+                                            _______,KC_PSCR,    _______, KC_P0,                                    \
+                                            _______,_______,    _______,_______,                                   \
+                                            _______,_______,    _______,_______,                                   \
+                                            _______,_______,    _______,_______                                    \
 ),
 
-[LAYER_SYMBOLS] = LAYOUT(
-    KC_RST, KC_EXLM,KC_PND, KC_DLR, KC_PERC,KC_AMPR,            KC_PEQL,KC_PPLS,KC_PMNS,KC_PAST,KC_PSLS,KC_CIRC,   \
-    XXXXXXX,KC_1,   KC_2,   KC_3,   KC_4,   KC_5,               KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   XXXXXXX,   \
-    XXXXXXX,KC_QUES,KC_LT,  KC_GT,  KC_AT,  KC_HASH,            KC_LBRC,KC_RBRC,KC_LPRN,KC_RPRN,KC_GRV, XXXXXXX,   \
-                    XXXXXXX,KC_TILD,                                            KC_LCBR,KC_RCBR,                   \
-                                _______,_______,                    _______,_______,                               \
-                                    _______,_______,            _______,_______,                                   \
-                                    _______,_______,            _______,_______                                    \
-),
-
-[LAYER_FUNCTION] = LAYOUT(
-    _______,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,            XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,KC_CRST,   \
-    XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,            XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,   \
-    XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,            XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,   \
-                    XXXXXXX,XXXXXXX,                                            XXXXXXX,XXXXXXX,                   \
-                                KC_VOLD,KC_VOLU,                    KC_BRIU,KC_BRID,                               \
-                                    KC_MPLY,QWERTY,             DVORAK, XXXXXXX,                                   \
-                                    KC_MUTE,XXXXXXX,            XXXXXXX,XXXXXXX                                    \
+[_RAISE] = LAYOUT(
+    _______,RESET,  _______,_______,_______,KC_LBRC,            KC_RBRC,_______,KC_NLCK,KC_INS, KC_SLCK,KC_MUTE,   \
+    _______,KC_LEFT,KC_UP  ,KC_DOWN,KC_RGHT,KC_LPRN,            KC_RPRN,KC_MPRV,KC_MPLY,KC_MNXT,_______,KC_VOLU,   \
+    _______,_______,_______,_______,_______,_______,            _______,_______,_______,_______,_______,KC_VOLD,   \
+                                            _______,_______,    KC_EQL, _______,                                   \
+                                            _______,_______,    _______,_______,                                   \
+                                            _______,_______,    _______,_______,                                   \
+                                            _______,_______,    _______,_______                                    \
 )
 };
 
 
 void persistent_default_layer_set(uint16_t default_layer) {
-    eeconfig_update_default_layer(default_layer);
-    default_layer_set(default_layer);
+  eeconfig_update_default_layer(default_layer);
+  default_layer_set(default_layer);
 }
 
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    if (keycode == KC_CUSTOM_RESET && record->event.pressed) {
-        // Send enter then reset the keyboard, so the flash script can proceed
-        register_code(KC_ENTER);
-        reset_keyboard();
-    }
-    return true;
-}
-
-void matrix_init_user(void) {
-    set_unicode_input_mode(UC_LNX);
-}
