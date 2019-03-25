a5f71a3ef435dbe5ba5520287b7b3f0755bae602
etherandrius <etherandrius@gmail.com>
2019-03-25 02:46:07 +0200

    some progress - compiles

--- keyboards/handwired/dactyl_manuform/4x6/keymaps/andrius/keymap.c
+++ keyboards/handwired/dactyl_manuform/4x6/keymaps/andrius/keymap.c
@@ -5,9 +5,10 @@ extern keymap_config_t keymap_config;
 // Layers
 enum {
     LAYER_QWERTY = 0,
-    LAYER_DVORAK,
-    LAYER_SYMBOLS,
     LAYER_ADJUST,
+    LAYER_LOWER,
+    LAYER_RAISE,
+    LAYER_FN,
 };
 
 // Custom keycodes
@@ -15,6 +16,9 @@ enum {
     KC_CUSTOM_RESET = SAFE_RANGE
 };
 #define KC_CRST KC_CUSTOM_RESET
+
+#define _______ KC_TRNS
+#define XXXXXXX KC_NO
 
 // Fillers to make layering more clear
 
@@ -26,27 +30,27 @@ enum {
 
 // Layer switches
 #define QWERTY DF(LAYER_QWERTY)
-#define DVORAK DF(LAYER_DVORAK)
-#define SYMBOLS MO(LAYER_SYMBOLS)
-#define SYMBLS SYMBOLS
+#define LOWER MO(LAYER_LOWER)
+#define RAISE MO(LAYER_RAISE)
+#define FN MO(LAYER_FN)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Base (qwerty)
  * +-----------------------------------------+                             +-----------------------------------------+
- * | TAB  |   q  |   w  |   e  |   r  |   t  |                             |   y  |   u  |   i  |   o  |   p  |      |
+ * | TAB  |   q  |   w  |   e  |   r  |   t  |                             |   y  |   u  |   i  |   o  |   p  |  `   |
  * |------+------+------+------+------+------|                             |------+------+------+------+------+------|
- * | ESC  |   a  |   s  |   d  |   f  |   g  |                             |   h  |   j  |   k  |   l  |   ;  |      |
+ * | ESC  |   a  |   s  |   d  |   f  |   g  |                             |   h  |   j  |   k  |   l  |   ;  |  '   |
  * |------+------+------+------+------+------|                             |------+------+------+------+------+------|
- * | SHFT |   z  |   x  |   c  |   v  |   b  |                             |   n  |   m  |   ,  |   .  |   /  |      |
+ * |SHIFT |   z  |   x  |   c  |   v  |   b  |                             |   n  |   m  |   ,  |   .  |   /  |SHIFT |
  * +------+------+------+------+-------------+                             +-------------+------+------+------+------+
- *               |  [   |   ]  |                                                         |      |      |
+ *               |   \  |   =  |                                                         | STIC |   -  |
  *               +-------------+-------------+                             +-------------+-------------+
- *                             |SPACE | ENT  |                             | BCSP | DEL  |
+ *                             |ETNER |SPACE |                             | BCSP | DEL  |
  *                             |------+------|                             |------+------|
  *                             |RAISE |LOWER |                             | CTRL | ALT  |
  *                             |------+------|                             |------+------|
- *                             | CMD  | META |                             | FN   |SUPER |
+ *                             | CMD  |SUPER |                             | FN   | META |
  *                             +-------------+                             +-------------+
  */
 
@@ -54,84 +58,75 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,  KC_Q,  KC_W,   KC_E,   KC_R,   KC_T,               KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_MINS,   \
     AD_ESC,  KC_A,  KC_S,   KC_D,   KC_F,   KC_G,               KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,   \
     KC_SCL,  KC_Z,  KC_X,   KC_C,   KC_V,   KC_B,               KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH, KC_SCL,   \
-                    KC_LBRC,KC_RBRC,                                            KC_PLUS,KC_EQL,                    \
-                                   KC_SPC , KC_ENT,             KC_BSPC,  KC_DEL,                                  \
-                                   _______, KC_LCTL,            KC_LALT,  _______,                                 \
-                                   _______, _______,            _______,  _______                                  \
+                    KC_BSLS,KC_EQL,                                             _______,KC_MINUS,                  \
+                                   KC_ENT , KC_SPC ,            KC_BSPC,  KC_DEL ,                                 \
+                                    RAISE ,  LOWER ,            KC_LCTL,  KC_LALT,                                 \
+                                   _______, _______,              FN   ,  KC_LGUI                                 \
 ),
-/* Base (qwerty)
- * +-----------------------------------------+                             +-----------------------------------------+
- * | TAB  |   q  |   w  |   e  |   r  |   t  |                             |   y  |   u  |  END |   o  |   p  |      |
- * |------+------+------+------+------+------|                             |------+------+------+------+------+------|
- * | ESC  | HOME |   s  |   d  |   f  |   g  |                             |   h  |   j  |   k  |   l  |   ;  |      |
- * |------+------+------+------+------+------|                             |------+------+------+------+------+------|
- * | SHFT |   z  |   x  |   c  |   v  |   b  |                             |   n  |   m  |   ,  |   .  |   /  |      |
- * +------+------+------+------+-------------+                             +-------------+------+------+------+------+
- *               |  [   |   ]  |                                                         |      |      |
- *               +-------------+-------------+                             +-------------+-------------+
- *                             |SPACE | ENT  |                             | BCSP | DEL  |
- *                             |------+------|                             |------+------|
- *                             |RAISE |LOWER |                             | CTRL | ALT  |
- *                             |------+------|                             |------+------|
- *                             | CMD  | META |                             | FN   |SUPER |
- *                             +-------------+                             +-------------+
- */
 
 /* Base (lower)
+ * +-----------------------------------------+                             +-----------------------------------------+
+ * | ESC  |   !  |   @  |   #  |   $  |   %  |                             |   ^  |   &  |   *  |   (  |   )  |      |
+ * |------+------+------+------+------+------|                             |------+------+------+------+------+------|
+ * | TAB  |      |      |      |      |      |                             | left | down | up   | right|      |      |
+ * |------+------+------+------+------+------|                             |------+------+------+------+------+------|
+ * | SHFT |      |      |      |      |      |                             |      |  [{  | }]   |  up  | right|      |
+ * +------+------+------+------+-------------+                             +-------------+------+------+------+------+
+ *               |      |      |                                                         | left | down |
+ *               +-------------+                                                         +-------------+
+ */
+[LAYER_LOWER] = LAYOUT(
+    _______,   KC_1,   KC_2,   KC_3,   KC_4,   KC_5,               KC_6,   KC_7,   KC_8,   KC_9,   KC_0,XXXXXXX,   \
+    _______,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,            KC_LEFT,KC_DOWN, KC_UP ,KC_RIGHT,XXXXXXX,XXXXXXX,  \
+    _______,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,            XXXXXXX,KC_LBRC,KC_RBRC, KC_UP , KC_RIGHT,_______, \
+                    XXXXXXX,XXXXXXX,                                            KC_LEFT,KC_DOWN,                   \
+                                    _______,_______,            _______,_______,                                   \
+                                    XXXXXXX,XXXXXXX,            _______,_______,                                   \
+                                    _______,_______,            _______,_______                                    \
+),
+
+/* Base (raise)
  * +-----------------------------------------+                             +-----------------------------------------+
  * | ESC  |   1  |   2  |   3  |   4  |   5  |                             |   6  |   7  |   8  |   9  |   0  |      |
  * |------+------+------+------+------+------|                             |------+------+------+------+------+------|
  * | TAB  |      |      |      |      |      |                             | left | down | up   | right|      |      |
  * |------+------+------+------+------+------|                             |------+------+------+------+------+------|
- * | SHFT |      |      |      |      |      |                             |      |      |      |      |      |      |
+ * | SHFT |      |      |      |      |      |                             |      |  [{  | }]   |  up  | right|      |
  * +------+------+------+------+-------------+                             +-------------+------+------+------+------+
- *               |  [   |   ]  |                                                         |      |      |
- *               +-------------+-------------+                             +-------------+-------------+
- *                             |SPACE | ENT  |                             | BCSP | DEL  |
- *                             |------+------|                             |------+------|
- *                             |RAISE |LOWER |                             | CTRL | ALT  |
- *                             |------+------|                             |------+------|
- *                             | CMD  | META |                             | FN   |SUPER |
- *                             +-------------+                             +-------------+
+ *               |      |      |                                                         | left | down |
+ *               +-------------+                                                         +-------------+
  */
+[LAYER_RAISE] = LAYOUT(
+    _______,   KC_1,   KC_2,   KC_3,   KC_4,   KC_5,               KC_6,   KC_7,   KC_8,   KC_9,   KC_0,XXXXXXX,   \
+    _______,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,            KC_LEFT,KC_DOWN, KC_UP ,KC_RIGHT,XXXXXXX,XXXXXXX,  \
+    _______,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,            XXXXXXX,KC_LBRC,KC_RBRC, KC_UP , KC_RIGHT,_______, \
+                    XXXXXXX,XXXXXXX,                                            KC_LEFT,KC_DOWN,                   \
+                                    _______,_______,            _______,_______,                                   \
+                                    XXXXXXX,XXXXXXX,            _______,_______,                                   \
+                                    _______,_______,            _______,_______                                    \
+),
 
 /* Base (fn)
  * +-----------------------------------------+                             +-----------------------------------------+
  * | ESC  |  f1  |  f2  |  f3  |  f4  |  f5  |                             |  f6  |  f7  |  f8  |  f9  | f10  |      |
  * |------+------+------+------+------+------|                             |------+------+------+------+------+------|
- * | TAB  |      |      |      |      |      |                             |      |      |      |      |      |      |
+ * | TAB  |      |      |      |      |      |                             | MUTE | VOL- | VOL+ | MUTE |      |      |
  * |------+------+------+------+------+------|                             |------+------+------+------+------+------|
- * | SHFT |      |      |      |      |      |                             |      |      |      |      |      |      |
+ * | SHFT |      |      |      |      |      |                             |      | BRI- | BRI+ |      |      |      |
  * +------+------+------+------+-------------+                             +-------------+------+------+------+------+
- *               |  [   |   ]  |                                                         |      |      |
- *               +-------------+-------------+                             +-------------+-------------+
- *                             |SPACE | ENT  |                             | BCSP | DEL  |
- *                             |------+------|                             |------+------|
- *                             |RAISE |LOWER |                             | CTRL | ALT  |
- *                             |------+------|                             |------+------|
- *                             | CMD  | META |                             | FN   |SUPER |
- *                             +-------------+                             +-------------+
+ *               |      |      |                                                         |      |      |
+ *               +-------------+                                                         +-------------+
  */
-
-[LAYER_DVORAK] = LAYOUT(
-    _______, KC_QUOT,KC_COMM,KC_DOT, KC_P,   KC_Y   ,            KC_F,    KC_G,  KC_C,   KC_R,   KC_L,   KC_UNDS,   \
-    KC_TAB, KC_A,   KC_O,   KC_E,   KC_U,   KC_I,               KC_D,    KC_H,  KC_T,   KC_N,   KC_S,   KC_COLN,   \
-    _______,KC_SCLN,KC_Q,   KC_J,   KC_K,   KC_X,               KC_B,    KC_M,  KC_W,   KC_V,   KC_Z,   KC_SLSH,   \
-                    _______,_______,                                            _______,_______,                   \
-                                    KC_SCL, KC_SPC,             KC_ENT, KC_BSPC,                                   \
-                                    KC_LALT,SYMBLS,             SYMBLS, KC_LCTL,                                   \
-                                    _______,KC_LWIN,            _______,KC_ESC                                     \
-),
-
-[LAYER_SYMBOLS] = LAYOUT(
-    _______, _______,_______,_______,_______,_______,            KC_PEQL,KC_PPLS,KC_PMNS,KC_PAST,KC_PSLS,KC_CIRC,   \
-    _______,KC_1,   KC_2,   KC_3,   KC_4,   KC_5,               KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   _______,   \
-    _______,_______,_______,_______,_______,_______,            KC_LBRC,KC_RBRC,KC_LPRN,KC_RPRN,_______,_______,   \
-                    _______,_______,                                            KC_LCBR,KC_RCBR,                   \
+[LAYER_FN] = LAYOUT(
+    _______,   KC_1,   KC_2,   KC_3,   KC_4,   KC_5,               KC_6,   KC_7,   KC_8,   KC_9,   KC_0,XXXXXXX,   \
+    _______,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,            KC_MUTE,KC_VOLD,KC_VOLU,KC_MUTE,XXXXXXX,XXXXXXX,   \
+    _______,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,            XXXXXXX,KC_BRID,KC_BRIU,XXXXXXX,XXXXXXX,_______,   \
+                    XXXXXXX,XXXXXXX,                                            XXXXXXX,XXXXXXX,                   \
                                     _______,_______,            _______,_______,                                   \
-                                    _______,_______,            _______,_______,                                   \
+                                    XXXXXXX,XXXXXXX,            _______,_______,                                   \
                                     _______,_______,            _______,_______                                    \
 ),
+
 
 [LAYER_ADJUST] = LAYOUT(
     _______,_______,_______,_______,_______,_______,            _______,_______,_______,_______,_______,KC_CRST,   \
@@ -140,7 +135,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                     _______,_______,                                            _______,_______,                   \
                                     _______,_______,            _______,_______,                                   \
                                     _______,_______,            _______,_______,                                   \
-                                    _______, QWERTY,             DVORAK,_______                                    \
+                                    _______,_______,            _______,_______                                    \
 )
 };
 
