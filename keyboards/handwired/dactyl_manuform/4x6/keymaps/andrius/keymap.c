54526bb16b4d5cd47a9e565d8a454971518275cd
etherandrius <etherandrius@gmail.com>
2019-03-29 15:37:38 +0200

    moved mouse wheel keys away from the thumb cluster

--- keyboards/handwired/dactyl_manuform/4x6/keymaps/andrius/keymap.c
+++ keyboards/handwired/dactyl_manuform/4x6/keymaps/andrius/keymap.c
@@ -167,13 +167,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *               +-------------+                                              +-------------+
  */
 [LAYER_MOUSE] = LAYOUT(
-    _______,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,           XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,   \
-    _______,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,           KC_MS_L,KC_MS_D,KC_MS_U,KC_MS_R,XXXXXXX,XXXXXXX,   \
+    _______,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,           XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,KC_WH_U,KC_WH_U,   \
+    _______,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,           KC_MS_L,KC_MS_D,KC_MS_U,KC_MS_R,KC_WH_D,KC_WH_D,   \
     QWERTY ,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,           XXXXXXX,XXXXXXX,XXXXXXX,KC_MS_U,KC_MS_R,QWERTY ,   \
                     XXXXXXX,XXXXXXX,                                            KC_MS_L,KC_MS_D,                  \
                                     _______,_______,            KC_BTN1,KC_BTN2,                                  \
-                                    _______,_______,            _______,_______,                                   \
-                                    _______,_______,            KC_WH_D,KC_WH_U                                  \
+                                    _______,_______,            _______,_______,                                  \
+                                    _______,_______,            _______,_______                                   \
 ),
 
 [LAYER_ADJUST] = LAYOUT(
