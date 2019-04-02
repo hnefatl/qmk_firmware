d12009a152fb3282d583c394b8c4779872715980
etherandrius <etherandrius@gmail.com>
2019-04-02 13:44:54 +0300

    added comma in the lower layer to write numbers easier

--- keyboards/handwired/dactyl_manuform/4x6/keymaps/andrius/keymap.c
+++ keyboards/handwired/dactyl_manuform/4x6/keymaps/andrius/keymap.c
@@ -82,7 +82,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|                  |------+------+------+------+------+------|
  * |      |   1  |   2  |   3  |   4  |   5  |                  | left | down | up   | right|      |      |
  * |------+------+------+------+------+------|                  |------+------+------+------+------+------|
- * |      |   6  |   7  |   8  |   9  |   0  |                  |      |  [{  | }]   |  up  | right|      |
+ * |      |   6  |   7  |   8  |   9  |   0  |                  |      |      |  ,   |  up  | right|      |
  * +------+------+------+------+-------------+                  +-------------+------+------+------+------+
  *               |      |      |                                              | left | down |
  *               +-------------+                                              +-------------+
@@ -90,7 +90,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [LAYER_LOWER] = LAYOUT(
     _______,   KC_1,   KC_2,   KC_3,   KC_4,   KC_5,               KC_6,   KC_7,   KC_8,   KC_9,   KC_0 ,XXXXXXX,  \
     _______,   KC_1,   KC_2,   KC_3,   KC_4,   KC_5,            KC_LEFT,KC_DOWN, KC_UP ,KC_RIGHT,XXXXXXX,XXXXXXX,  \
-    _______,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,            XXXXXXX,KC_LBRC,KC_RBRC, KC_UP ,KC_RIGHT,_______,  \
+    _______,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,            XXXXXXX,XXXXXXX,_______, KC_UP ,KC_RIGHT,_______,  \
                     XXXXXXX,XXXXXXX,                                            KC_LEFT,KC_DOWN,                   \
                                     _______,_______,            _______,_______,                                   \
                                     _______,XXXXXXX,             RA_LO ,_______,                                   \
