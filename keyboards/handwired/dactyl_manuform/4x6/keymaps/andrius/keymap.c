c1d3c30707ddd817237106c7172164bca153208f
etherandrius <etherandrius@gmail.com>
2019-04-07 18:30:58 +0300

    swapped = and -

--- keyboards/handwired/dactyl_manuform/4x6/keymaps/andrius/keymap.c
+++ keyboards/handwired/dactyl_manuform/4x6/keymaps/andrius/keymap.c
@@ -56,7 +56,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|                  |------+------+------+------+------+------|
  * |SHIFT |   z  |   x  |   c  |   v  |   b  |                  |   n  |   m  |   ,  |   .  |   /  |SHIFT |
  * +------+------+------+------+-------------+                  +-------------+------+------+------+------+
- *               |   \  |   =  |                                              |LEADER|   -  |
+ *               |   \  |   -  |                                              |LEADER|   =  |
  *               +-------------+-------------+                  +-------------+-------------+
  *                             |ETNER |SPACE |                  | BCSP | DEL  |
  *                             |------+------|                  |------+------|
@@ -70,7 +70,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB ,  KC_Q,  KC_W,   KC_E,   KC_R,   KC_T,               KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_GRV,   \
     AD_ESC ,  KC_A,  KC_S,   KC_D,   KC_F,   KC_G,               KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,  \
     KC_LSFT,  KC_Z,  KC_X,   KC_C,   KC_V,   KC_B,               KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH, KC_RSFT, \
-                     KC_BSLS,KC_EQL,                                             MOUSE  ,KC_MINUS,                 \
+                     KC_BSLS,KC_MINUS,                                             MOUSE  ,KC_EQL,                 \
                                     KC_ENT , KC_SPC ,            KC_BSPC,  KC_DEL ,                                \
                                     KC_LCTL,  LOWER ,             RAISE ,  KC_LALT,                                \
                                     KC_PSCR,  TEST  ,              FN   ,  KC_LGUI                                 \
