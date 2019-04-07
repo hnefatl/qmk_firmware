373bed35858fb6f42044f2945a148baf4c4f101b
etherandrius <etherandrius@gmail.com>
2019-04-07 11:57:55 +0300

    added <> brackets fixed desc

--- keyboards/handwired/dactyl_manuform/4x6/keymaps/andrius/keymap.c
+++ keyboards/handwired/dactyl_manuform/4x6/keymaps/andrius/keymap.c
@@ -99,18 +99,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Base (raise)
  * |------+------+------+------+------+------|                  |------+------+------+------+------+------|
- * |      |      |      |  !   |  ^   |      |                  |      |      |      |      |      |      |
+ * |      |      |      |  !   |  ^   |      |                  |  {   |  }   |  <   | >    |      |      |
  * |------+------+------+------+------+------|                  |------+------+------+------+------+------|
- * |      |  @   |  *   |  $   |  %   |  &   |                  |  (   |  )   |      |      |      |      |
+ * |      |  @   |  *   |  $   |  %   |  &   |                  |  (   |  )   |  <   | >    |      |      |
  * |------+------+------+------+------+------|                  |------+------+------+------+------+------|
- * |      |      |      |  &   |  #   |      |                  |  [{  |  }]  |      |  up  | right|      |
+ * |      |      |      |  &   |  #   |      |                  |  [   |  ]   |      |  up  | right|      |
  * +------+------+------+------+-------------+                  +-------------+------+------+------+------+
  *               |      |      |                                              | left | down |
  *               +-------------+                                              +-------------+
  */
 [LAYER_RAISE] = LAYOUT(
-    _______,XXXXXXX,XXXXXXX,S(KC_1),S(KC_6),XXXXXXX,            KC_LCBR,KC_RCBR,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,   \
-    _______,S(KC_2),S(KC_8),S(KC_4),S(KC_5),S(KC_7),            S(KC_9),S(KC_0),XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,  \
+    _______,XXXXXXX,XXXXXXX,S(KC_1),S(KC_6),XXXXXXX,            KC_LCBR,KC_RCBR,KC_LABK,KC_RABK,XXXXXXX,XXXXXXX,   \
+    _______,S(KC_2),S(KC_8),S(KC_4),S(KC_5),S(KC_7),            S(KC_9),S(KC_0),KC_LABK,KC_RABK,XXXXXXX,XXXXXXX,  \
     _______,XXXXXXX,XXXXXXX,S(KC_7),S(KC_3),XXXXXXX,            KC_LBRC,KC_RBRC,XXXXXXX,KC_UP ,KC_RIGHT,_______,  \
                     XXXXXXX,XXXXXXX,                                            KC_LEFT,KC_DOWN,                   \
                                     _______,_______,            _______,_______,                                   \
