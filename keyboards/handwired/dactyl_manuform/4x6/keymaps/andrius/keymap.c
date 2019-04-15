66bd2bb404b23b13feb3a478e887609ca0b0796b
etherandrius <etherandrius@gmail.com>
2019-04-15 17:20:05 +0300

    del and grave

--- keyboards/handwired/dactyl_manuform/4x6/keymaps/andrius/keymap.c
+++ keyboards/handwired/dactyl_manuform/4x6/keymaps/andrius/keymap.c
@@ -59,7 +59,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Base (qwerty)
  * +-----------------------------------------+                  +-----------------------------------------+
- * | TAB  |   q  |   w  |   e  |   r  |   t  |                  |   y  |   u  |   i  |   o  |   p  |  `   |
+ * | `    |   q  |   w  |   e  |   r  |   t  |                  |   y  |   u  |   i  |   o  |   p  | DEL  |
  * |------+------+------+------+------+------|                  |------+------+------+------+------+------|
  * | ESC  |   a  |   s  |   d  |   f  |   g  |                  |   h  |   j  |   k  |   l  |   ;  |  '   |
  * |------+------+------+------+------+------|                  |------+------+------+------+------+------|
@@ -76,7 +76,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
 [LAYER_QWERTY] = LAYOUT(
-    KC_TAB ,  KC_Q,  KC_W,   KC_E,   KC_R,   KC_T,               KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_GRV,   \
+    KC_GRV ,  KC_Q,  KC_W,   KC_E,   KC_R,   KC_T,               KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_DEL,   \
     AD_ESC ,  KC_A,  KC_S,   KC_D,   KC_F,   KC_G,               KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,  \
     KC_LSFT,  KC_Z,  KC_X,   KC_C,   KC_V,   KC_B,               KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,SF_ESC,   \
                      KC_BSLS,KC_MINUS,                                             MOUSE  ,KC_EQL,                 \
