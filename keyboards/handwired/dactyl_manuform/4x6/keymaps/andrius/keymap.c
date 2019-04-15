e9ac5d14126cd0193ca005efec0da647c5a9b03e
etherandrius <etherandrius@gmail.com>
2019-04-15 17:16:39 +0300

    tab instead of delete

--- keyboards/handwired/dactyl_manuform/4x6/keymaps/andrius/keymap.c
+++ keyboards/handwired/dactyl_manuform/4x6/keymaps/andrius/keymap.c
@@ -67,7 +67,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * +------+------+------+------+-------------+                  +-------------+------+------+------+------+
  *               |   \  |   -  |                                              | MOUSE|   =  |
  *               +-------------+-------------+                  +-------------+-------------+
- *                             |ETNER |SPACE |                  | BCSP | DEL  |
+ *                             |ETNER |SPACE |                  | BCSP | TAB  |
  *                             |------+------|                  |------+------|
  *                             | CTRL |LOWER |                  |RAISE | ALT  |
  *                             |------+------|                  |------+------|
@@ -80,7 +80,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     AD_ESC ,  KC_A,  KC_S,   KC_D,   KC_F,   KC_G,               KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,  \
     KC_LSFT,  KC_Z,  KC_X,   KC_C,   KC_V,   KC_B,               KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,SF_ESC,   \
                      KC_BSLS,KC_MINUS,                                             MOUSE  ,KC_EQL,                 \
-                                    KC_ENT , KC_SPC ,            KC_BSPC,  KC_DEL ,                                \
+                                    KC_ENT , KC_SPC ,            KC_BSPC,  KC_TAB ,                                \
                                     KC_LCTL,  LOWER ,             RAISE ,  KC_LALT,                                \
                                     KC_LSFT,  TEST  ,              FN   ,  KC_LGUI                                 \
 ),
@@ -101,7 +101,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______,   KC_1,   KC_2,   KC_3,   KC_4,   KC_5,            KC_LEFT,KC_DOWN, KC_UP ,KC_RIGHT,_______,_______, \
     _______,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,            XXXXXXX,XXXXXXX,_______,_______,_______,_______,  \
                     XXXXXXX,XXXXXXX,                                            XXXXXXX,XXXXXXX,                   \
-                                    _______,_______,            _______,_______,                                   \
+                                    _______,_______,            KC_DEL ,KC_DEL ,                                   \
                                     _______,XXXXXXX,             RA_LO ,_______,                                   \
                                     _______,_______,            _______,_______                                    \
 ),
